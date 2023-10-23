2c1d54182819b323cf02eabc89024b519a22028c
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-10-22 20:22:16 -0400

    bookings get endpoint

---
+++ pages/api/bookings.ts
@@ -0,0 +1,47 @@
+const { connectToDatabase } = require('../../lib/mongodb');
+const ObjectId = require('mongodb').ObjectId;
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type ResponseData = {
+  message: string
+}
+
+const collectionName = "bookings"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+
+  switch (req.method) {
+    case 'GET': {
+      return getBookings(req, res);
+    }
+    case 'OPTIONS': {
+      return res.status(200).send({message: 'ok'});
+    }
+  }
+
+  async function getBookings(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+  ){
+    try {
+      let { db } = await connectToDatabase();
+      let rooms = await db
+          .collection(collectionName)
+          .find()
+          .toArray();
+      return res.json({
+        message: JSON.parse(JSON.stringify(rooms)),
+        success: true,
+      });
+    } catch (error) {
+      // return the error
+      return res.json({
+        message: new Error(error as any).message,
+        success: false,
+      });
+    }
+  }
+}
