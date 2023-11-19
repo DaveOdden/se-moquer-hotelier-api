1cda95a5a1474ad54fefd721716fc9e878278cd4
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-11-18 23:03:51 -0500

    add settings endpoint

---
+++ pages/api/settings.ts
@@ -0,0 +1,58 @@
+const { connectToDatabase } = require('../../lib/mongodb');
+const ObjectId = require('mongodb').ObjectId;
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type ResponseData = {
+  message: string
+}
+
+const collectionName = "settings"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+
+  switch (req.method) {
+    case 'GET': {
+      return getSettings(req, res);
+    }
+    case 'OPTIONS': {
+      return res.status(200).send({message: 'ok'});
+    }
+  }
+
+  async function getSettings(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+  ){
+    try {
+      let { db } = await connectToDatabase();
+      let settings;
+
+      if(req.query.id) {
+        settings = await db
+          .collection(collectionName)
+          .findOne({
+            _id: parseInt(req.query.id as any)
+          })
+      } else {
+        settings = await db
+          .collection(collectionName)
+          .find()
+          .sort({_id:1})
+          .toArray(); 
+      }
+
+      return res.json({
+        message: JSON.parse(JSON.stringify(settings)),
+        success: true,
+      });
+    } catch (error) {
+      return res.json({
+        message: new Error(error as any).message,
+        success: false,
+      });
+    }
+  }
+}
