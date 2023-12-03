71b3422df9681a0a12f3b8ac5ce9602fc10e8e91
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-03 00:36:57 -0500

    remove authorization

--- middleware.js
+++ middleware.js
@@ -2,19 +2,19 @@ import { headers } from 'next/headers'
 import { NextResponse, userAgent } from 'next/server'
 
 export function middleware(req, ev) {
-  const headersInstance = headers()
-  const authorization = headersInstance.get('authorization')
-  if (!authorization || authorization !== process.env.DATA_API_KEY) {
-    return NextResponse.json(
-      {
-        message: "Unauthorized",
-      },
-      {
-        status: 401,
-      }
-    );
-  }
+  // const headersInstance = headers()
+  // const authorization = headersInstance.get('authorization')
+  // if (!authorization || authorization !== process.env.DATA_API_KEY) {
+  //   return NextResponse.json(
+  //     {
+  //       message: "Unauthorized",
+  //     },
+  //     {
+  //       status: 401,
+  //     }
+  //   );
+  // }
 
-  return NextResponse.next();
+  // return NextResponse.next();
 } 
 
