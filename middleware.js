19d110812dd761a628370b7fdca0d2920d766fc5
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-03 01:46:57 -0500

    return on options

--- middleware.js
+++ middleware.js
@@ -2,6 +2,9 @@ import { headers } from 'next/headers'
 import { NextResponse, userAgent } from 'next/server'
 
 export function middleware(req, ev) {
+  if(req.method === "OPTIONS") {
+    return res.status(200).send({ message: 'ok' });
+  }
   const headersInstance = headers()
   const authorization = headersInstance.get('Authorization')
   if (!authorization || authorization !== process.env.DATA_API_KEY) {
