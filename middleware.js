28c7bcc56474918dd079430f756fe7e75942c0fe
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-02 23:57:11 -0500

    require data api key

---
+++ middleware.js
@@ -0,0 +1,20 @@
+import { headers } from 'next/headers'
+import { NextResponse, userAgent } from 'next/server'
+
+export function middleware(req, ev) {
+  const headersInstance = headers()
+  const authorization = headersInstance.get('authorization')
+  if (!authorization || authorization !== process.env.DATA_API_KEY) {
+    return NextResponse.json(
+      {
+        message: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  return NextResponse.next();
+} 
+
