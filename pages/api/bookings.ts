418620091bbceed0465fa25c819ff0c40169a818
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-10 12:14:40 -0500

    add guest datesOfStay

--- pages/api/bookings.ts
+++ pages/api/bookings.ts
@@ -1,8 +1,8 @@
-const { connectToDatabase } = require('../../lib/mongodb');
-const dayjs = require('dayjs')
-const util = require('./util/util')
-const ObjectId = require('mongodb').ObjectId;
-import type { NextApiRequest, NextApiResponse } from 'next'
+const { connectToDatabase } = require("../../lib/mongodb")
+const dayjs = require("dayjs")
+const util = require("./util/util")
+const ObjectId = require("mongodb").ObjectId
+import type { NextApiRequest, NextApiResponse } from "next"
 
 type ResponseData = {
   message: string
@@ -14,149 +14,152 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-
   switch (req.method) {
-    case 'GET': {
-      return getBookings(req, res);
+    case "GET": {
+      return getBookings(req, res)
     }
-    case 'POST': {
-      return addBooking(req, res);
+    case "POST": {
+      return addBooking(req, res)
     }
-    case 'PUT': {
-      return updateBooking(req, res);
+    case "PUT": {
+      return updateBooking(req, res)
     }
-    case 'DELETE': {
-      return deleteBooking(req, res);
+    case "DELETE": {
+      return deleteBooking(req, res)
     }
-    case 'OPTIONS': {
-      return res.status(200).send({ message: 'ok' });
+    case "OPTIONS": {
+      return res.status(200).send({ message: "ok" })
     }
   }
 
-  async function getBookings(
-    req: NextApiRequest,
-    res: NextApiResponse<any>
-  ) {
+  async function getBookings(req: NextApiRequest, res: NextApiResponse<any>) {
     try {
-      let { db } = await connectToDatabase();
-      let rooms = await db
-        .collection(collectionName)
-        .find()
-        .toArray();
+      let { db } = await connectToDatabase()
+      let rooms = await db.collection(collectionName).find().toArray()
 
-      let arrayOfRoomsBooked: Array<any> = [];
+      let arrayOfRoomsBooked: Array<any> = []
       rooms.forEach((record: any) => {
         if (record.room._id) {
           arrayOfRoomsBooked.push(record.room._id)
         }
-      });
+      })
 
       return res.json({
         message: JSON.parse(JSON.stringify(rooms)),
         arrayOfRoomsBooked: arrayOfRoomsBooked,
         success: true,
-      });
+      })
     } catch (error) {
       return res.json({
         message: new Error(error as any).message,
         success: false,
-      });
+      })
     }
   }
 
-  async function addBooking(
-    req: NextApiRequest,
-    res: NextApiResponse<any>
-  ) {
+  async function addBooking(req: NextApiRequest, res: NextApiResponse<any>) {
     try {
-      let { db } = await connectToDatabase();
-      let data = JSON.parse(req.body);
-      let guestId = data.guest;
-      let roomId = data.room;
+      let { db } = await connectToDatabase()
+      let data = JSON.parse(req.body)
+      let guestId = data.guest
+      let roomId = data.room
       data.guest = {
-        _id: new ObjectId(guestId)
+        _id: new ObjectId(guestId),
       }
       data.room = {
-        _id: roomId
+        _id: roomId,
       }
 
       const addOneDayToDate = (_date: any) => {
         const a = dayjs(_date)
-        return a.add(1, 'day')
+        return a.add(1, "day")
       }
 
-      let arrayOfDatesBooked: Array<any> = [];
+      let arrayOfDatesBooked: Array<any> = []
 
-      if (dayjs(data.checkinDate).isSame(dayjs(data.checkoutDate), 'day')) {
-        arrayOfDatesBooked.push(dayjs(data.checkinDate).format('YYYY-MM-DD'))
+      if (dayjs(data.checkinDate).isSame(dayjs(data.checkoutDate), "day")) {
+        arrayOfDatesBooked.push(dayjs(data.checkinDate).format("YYYY-MM-DD"))
       }
-      if (dayjs(data.checkinDate).isBefore(dayjs(data.checkoutDate), 'day')) {
-        var dateWithinRange = true;
-        var cyclingDate = dayjs(data.checkinDate);
+      if (dayjs(data.checkinDate).isBefore(dayjs(data.checkoutDate), "day")) {
+        var dateWithinRange = true
+        var cyclingDate = dayjs(data.checkinDate)
         while (dateWithinRange) {
-          if (dayjs(cyclingDate).isSame(dayjs(data.checkoutDate), 'day')) {
-            arrayOfDatesBooked.push(dayjs(cyclingDate).format('YYYY-MM-DD'))
-            dateWithinRange = false;
-            break;
-          } else if (dayjs(cyclingDate).isBefore(dayjs(data.checkoutDate), 'day')) {
-            arrayOfDatesBooked.push(dayjs(cyclingDate).format('YYYY-MM-DD'));
-            cyclingDate = addOneDayToDate(cyclingDate);
+          if (dayjs(cyclingDate).isSame(dayjs(data.checkoutDate), "day")) {
+            arrayOfDatesBooked.push(dayjs(cyclingDate).format("YYYY-MM-DD"))
+            dateWithinRange = false
+            break
+          } else if (
+            dayjs(cyclingDate).isBefore(dayjs(data.checkoutDate), "day")
+          ) {
+            arrayOfDatesBooked.push(dayjs(cyclingDate).format("YYYY-MM-DD"))
+            cyclingDate = addOneDayToDate(cyclingDate)
           }
         }
-
       }
 
       // update corresponding room record with dates
-      await db.collection("rooms").updateOne({
-        _id: roomId
-      }, {
-        $push: { "datesBooked": { $each: arrayOfDatesBooked } }
-      });
+      await db.collection("rooms").updateOne(
+        {
+          _id: roomId,
+        },
+        {
+          $push: { datesBooked: { $each: arrayOfDatesBooked } },
+        }
+      )
+
+      // update corresponding guest record with dates
+      await db.collection("guests").updateOne(
+        {
+          _id: guestId,
+        },
+        {
+          $push: { datesOfStay: { $each: arrayOfDatesBooked } },
+        }
+      )
 
       // update corresponding guest record with history
-      let bookingData = structuredClone(data);
-      delete bookingData.guest;
-      await db.collection("guests").updateOne({
-        _id: new ObjectId(guestId)
-      }, {
-        $push: { history: { action: 'New Booking', booking: bookingData } }
-      });
+      let bookingData = structuredClone(data)
+      delete bookingData.guest
+      await db.collection("guests").updateOne(
+        {
+          _id: new ObjectId(guestId),
+        },
+        {
+          $push: { history: { action: "New Booking", booking: bookingData } },
+        }
+      )
 
       // insert booking
-      await db.collection(collectionName).insertOne(data);
+      await db.collection(collectionName).insertOne(data)
 
       return res.json({
-        message: 'booking added successfully',
+        message: "booking added successfully",
         success: true,
-      });
+      })
     } catch (error) {
       return res.json({
         message: new Error(error as any).message,
         success: false,
-      });
+      })
     }
   }
 
-  async function updateBooking(
-    req: NextApiRequest,
-    res: NextApiResponse<any>
-  ) {
+  async function updateBooking(req: NextApiRequest, res: NextApiResponse<any>) {
     try {
-      let { db } = await connectToDatabase();
+      let { db } = await connectToDatabase()
       let bodyJson = JSON.parse(req.body)
       let newData = structuredClone(bodyJson)
       // delete newData._id
-      let roomId = bodyJson.room;
+      let guestId = bodyJson.guest
+      let roomId = bodyJson.room
       newData.room = {
-        _id: roomId
+        _id: roomId,
       }
 
       // get original booking data
-      let thisBooking = await db
-        .collection(collectionName)
-        .findOne({
-          _id: new ObjectId(req.query.id)
-        })
+      let thisBooking = await db.collection(collectionName).findOne({
+        _id: new ObjectId(req.query.id),
+      })
 
       if (newData.checkinDate === undefined) {
         newData.checkinDate = thisBooking.checkinDate
@@ -170,122 +173,170 @@ export default async function handler(
       let newDatesBooked = util.getArrayOfDatesBooked(newData) // **** checkinDate and checkoutDate EXPECTED IN PAYLOAD
 
       // update booking
-      let dbResult = await db.collection(collectionName).updateOne({
-        _id: new ObjectId(req.query.id)
-      }, {
-        $set: newData
-      });
+      let dbResult = await db.collection(collectionName).updateOne(
+        {
+          _id: new ObjectId(req.query.id),
+        },
+        {
+          $set: newData,
+        }
+      )
 
-      console.log(originalDatesBooked)
       // remove original booked dates from room's datesBooked array
-      let removeDatesFromRoom;
-      console.log(thisBooking.room._id)
+      let removeDatesFromRoom
       if (thisBooking.room._id >= 0) {
-        removeDatesFromRoom = await db.collection('rooms').updateOne({
-          _id: parseInt(thisBooking.room._id),
-        }, {
-          $pull: { datesBooked: { $in: originalDatesBooked } },
-        });
+        removeDatesFromRoom = await db.collection("rooms").updateOne(
+          {
+            _id: parseInt(thisBooking.room._id),
+          },
+          {
+            $pull: { datesBooked: { $in: originalDatesBooked } },
+          }
+        )
       }
 
-      console.log(newDatesBooked)
-      console.log(bodyJson.room)
+      // remove original booked dates from room's datesBooked array
+      let removeDatesFromGuest
+      if (thisBooking.room._id >= 0) {
+        removeDatesFromGuest = await db.collection("guests").updateOne(
+          {
+            _id: new ObjectId(guestId),
+          },
+          {
+            $pull: { datesOfStay: { $in: originalDatesBooked } },
+          }
+        )
+      }
+
       // add booked dates to new room ***** ROOM IS EXPECTED IN PAYLOAD
-      let addDatesToNewRoom = await db.collection('rooms').updateOne({
-        _id: parseInt(bodyJson.room),
-      }, {
-        $push: { datesBooked: { $each: newDatesBooked } }
-      });
-      console.log(addDatesToNewRoom)
+      let addDatesToNewRoom = await db.collection("rooms").updateOne(
+        {
+          _id: parseInt(bodyJson.room),
+        },
+        {
+          $push: { datesBooked: { $each: newDatesBooked } },
+        }
+      )
+
+      // add booked dates to new room ***** GUEST IS EXPECTED IN PAYLOAD
+      let addDatesToNewGuest = await db.collection("guests").updateOne(
+        {
+          _id: new ObjectId(guestId),
+        },
+        {
+          $push: { datesOfStay: { $each: newDatesBooked } },
+        }
+      )
 
       // update guest history
-      await db.collection("guests").updateOne({
-        _id: new ObjectId(thisBooking.guest._id)
-      }, {
-        $push: { history: { action: 'Booking Updated', booking: bodyJson } }
-      });
+      await db.collection("guests").updateOne(
+        {
+          _id: new ObjectId(thisBooking.guest._id),
+        },
+        {
+          $push: { history: { action: "Booking Updated", booking: bodyJson } },
+        }
+      )
 
       return res.json({
-        message: 'Booking updated successfully',
+        message: "Booking updated successfully",
         success: true,
-      });
+      })
     } catch (error) {
       return res.json({
         message: new Error(error as any).message,
         success: false,
-      });
+      })
     }
   }
 
-  async function deleteBooking(
-    req: NextApiRequest,
-    res: NextApiResponse<any>
-  ) {
+  async function deleteBooking(req: NextApiRequest, res: NextApiResponse<any>) {
     try {
-      let { db } = await connectToDatabase();
-      let roomInfo: any;
+      let { db } = await connectToDatabase()
+      let roomInfo: any
 
       const addOneDayToDate = (_date: any) => {
         const a = dayjs(_date)
-        return a.add(1, 'day')
+        return a.add(1, "day")
       }
 
-      let bookingInfo = await db
-        .collection(collectionName)
-        .findOne({
-          _id: new ObjectId(req.query.id)
-        })
+      let bookingInfo = await db.collection(collectionName).findOne({
+        _id: new ObjectId(req.query.id),
+      })
 
-      let arrayOfDatesBooked: Array<string> = [];
-      if (dayjs(bookingInfo.checkinDate).isSame(dayjs(bookingInfo.checkoutDate), 'day')) {
-        arrayOfDatesBooked.push(dayjs(bookingInfo.checkinDate).format('YYYY-MM-DD'))
+      let arrayOfDatesBooked: Array<string> = []
+      if (
+        dayjs(bookingInfo.checkinDate).isSame(
+          dayjs(bookingInfo.checkoutDate),
+          "day"
+        )
+      ) {
+        arrayOfDatesBooked.push(
+          dayjs(bookingInfo.checkinDate).format("YYYY-MM-DD")
+        )
       }
-      if (dayjs(bookingInfo.checkinDate).isBefore(dayjs(bookingInfo.checkoutDate), 'day')) {
-        var dateWithinRange = true;
-        var cyclingDate = dayjs(bookingInfo.checkinDate);
+      if (
+        dayjs(bookingInfo.checkinDate).isBefore(
+          dayjs(bookingInfo.checkoutDate),
+          "day"
+        )
+      ) {
+        var dateWithinRange = true
+        var cyclingDate = dayjs(bookingInfo.checkinDate)
         while (dateWithinRange) {
-          if (dayjs(cyclingDate).isSame(dayjs(bookingInfo.checkoutDate), 'day')) {
-            arrayOfDatesBooked.push(dayjs(cyclingDate).format('YYYY-MM-DD'))
-            dateWithinRange = false;
-            break;
-          } else if (dayjs(cyclingDate).isBefore(dayjs(bookingInfo.checkoutDate), 'day')) {
-            arrayOfDatesBooked.push(dayjs(cyclingDate).format('YYYY-MM-DD'));
-            cyclingDate = addOneDayToDate(cyclingDate);
+          if (
+            dayjs(cyclingDate).isSame(dayjs(bookingInfo.checkoutDate), "day")
+          ) {
+            arrayOfDatesBooked.push(dayjs(cyclingDate).format("YYYY-MM-DD"))
+            dateWithinRange = false
+            break
+          } else if (
+            dayjs(cyclingDate).isBefore(dayjs(bookingInfo.checkoutDate), "day")
+          ) {
+            arrayOfDatesBooked.push(dayjs(cyclingDate).format("YYYY-MM-DD"))
+            cyclingDate = addOneDayToDate(cyclingDate)
           }
         }
       }
 
-      let removeDatesFromRoom;
+      let removeDatesFromRoom
       if (bookingInfo.room._id >= 0) {
-        removeDatesFromRoom = await db.collection('rooms').updateOne({
-          _id: parseInt(bookingInfo.room._id),
-        }, {
-          $pull: { datesBooked: { $in: arrayOfDatesBooked } }
-        });
+        removeDatesFromRoom = await db.collection("rooms").updateOne(
+          {
+            _id: parseInt(bookingInfo.room._id),
+          },
+          {
+            $pull: { datesBooked: { $in: arrayOfDatesBooked } },
+          }
+        )
       }
 
       if (removeDatesFromRoom.modifiedCount && bookingInfo.guest._id) {
-        await db.collection("guests").updateOne({
-          _id: new ObjectId(bookingInfo.guest._id)
-        }, {
-          $push: { history: { action: 'Cancelled Booking', booking: bookingInfo } }
-        });
+        await db.collection("guests").updateOne(
+          {
+            _id: new ObjectId(bookingInfo.guest._id),
+          },
+          {
+            $push: {
+              history: { action: "Cancelled Booking", booking: bookingInfo },
+            },
+          }
+        )
 
         await db.collection(collectionName).deleteOne({
           _id: new ObjectId(req.query.id),
-        });
+        })
       }
 
       return res.json({
-        message: 'Booking deleted successfully',
+        message: "Booking deleted successfully",
         success: true,
-      });
+      })
     } catch (error) {
       return res.json({
         message: new Error(error as any).message,
         success: false,
-      });
+      })
     }
   }
-
 }
