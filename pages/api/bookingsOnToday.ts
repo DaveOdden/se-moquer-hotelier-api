8725150603cb4d6393068909716072dbb1e20d28
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-14 22:46:07 -0500

    add logs for date on server

--- pages/api/bookingsOnToday.ts
+++ pages/api/bookingsOnToday.ts
@@ -28,6 +28,20 @@ export default async function handler(
   ) {
     try {
       let { db } = await connectToDatabase()
+      let start = new Date()
+      let end = new Date()
+      // console.log("")
+      // console.log(start)
+      // console.log(end)
+      console.log(dayjs().startOf("day").format("hh-mm-ss"))
+      console.log(dayjs().endOf("day").format("hh-mm-ss"))
+
+      // start.setHours(0, 0, 0, 0)
+      // end.setHours(23, 59, 59, 999)
+      console.log(start)
+      console.log(end)
+      console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
+
       let bookings = await db
         .collection(collectionName)
         .find({
