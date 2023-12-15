cd464c78e97b68589967d18105de0142b89dd501
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-15 00:07:46 -0500

    back to uytc

--- pages/api/bookings.ts
+++ pages/api/bookings.ts
@@ -71,8 +71,8 @@ export default async function handler(
         _id: roomId,
       }
 
-      data.checkinDate = dayjs(data.checkinDate).utc().toISOString()
-      data.checkoutDate = dayjs(data.checkoutDate).utc().toISOString()
+      data.checkinDate = dayjs(data.checkinDate).utc()
+      data.checkoutDate = dayjs(data.checkoutDate).utc()
       console.log(data.checkinDate)
       console.log(data.checkoutDate)
 
