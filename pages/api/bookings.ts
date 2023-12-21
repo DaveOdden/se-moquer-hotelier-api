778677735cfaa12dcddc84460a7d45a2b1a043e8
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-21 10:27:27 -0500

    test

--- pages/api/bookings.ts
+++ pages/api/bookings.ts
@@ -100,6 +100,7 @@ export default async function handler(
       console.log(dayjs(data.checkinDate).format())
       console.log(dayjs().utc(data.checkinDate).format())
       console.log(dayjs(data.checkinDate).utc().format())
+      console.log(dayjs(data.checkinDate).utcOffset(utcOffset).format())
       console.log(dayjs().utc(data.checkinDate).utcOffset(utcOffset).format())
 
       console.log(
